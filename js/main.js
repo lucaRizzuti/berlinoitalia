7f1b023fb745cddde9e632fc864f3578aeef575d
luca rizzuti <luca.rizzuti@z-lab.com>
2020-06-22 13:21:06 +0200

    aggiunto analytic e ripulita la directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function parseMarkdown() {
-    var items = document.getElementsByClassName('parse-as-markdown');
-
-    for (var i = 0; i < items.length; i++) {
-        items[i].innerHTML = marked(items[i].textContent);
-    }
-}
-
-parseMarkdown();
